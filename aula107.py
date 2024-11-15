1dd115c1db55e3a7bb981ca5811038e0251741fa
Luis Henrique <62081612+luisitcho@users.noreply.github.com>
2024-11-15 15:20:47 -0300

    Add files via upload

---
+++ aula107.py
@@ -0,0 +1,20 @@
+# from aula107_package.modulo import soma_do_modulo
+# from aula107_package import modulo
+# import aula107_package.modulo
+# from aula107_package.modulo import *
+
+# print(soma_do_modulo(2, 2))
+# print(variavel)
+# print(modulo.soma_do_modulo(5, 5))
+# print(aula107_package.modulo.soma_do_modulo(1, 1))
+# print(__name__)
+
+# from aula107_package.modulo import soma_do_modulo
+
+# print(soma_do_modulo(2, 2))
+
+import aula107_package
+
+print(aula107_package.dobra_numero(4))
+print(aula107_package.variavel)
+print(aula107_package.soma_do_modulo(2, 2))
