1bfea7cbee146873c2ac7a0672b49aafc8ed18dc
luisitcho <luishenrisc1@gmail.com>
2024-11-15 18:33:00 -0300

    curso python - aula 115

--- aula115.py
+++ aula115.py
@@ -11,5 +11,40 @@ lista_b     = [1, 2, 3, 4]
 lista_soma  = [2, 4, 6, 8]
 """
 
-lista_a     = [1, 2, 3, 4, 5, 6, 7]
-lista_b     = [1, 2, 3, 4]
+'''
+Minha solução
+'''
+
+lista_a = [1, 2, 3, 4, 5, 6, 7]
+lista_b = [1, 2, 3, 4]
+
+# def zipper(l1, l2):
+#     intervalo = min(len(l1), len(l2))
+
+#     return [
+#         l1[i] + l2[i]
+#         for i in range(intervalo)
+#     ]
+
+
+# print(zipper(lista_a, lista_b))
+
+
+'''
+Solução definitiva
+'''
+
+lista_a = [10, 2, 3, 40, 5, 6, 7]
+lista_b = [1, 2, 3, 4]
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)
+
+# lista_soma = []
+# for i in range(len(lista_b)):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
+
+# lista_soma = []
+# for i, _ in enumerate(lista_b):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
