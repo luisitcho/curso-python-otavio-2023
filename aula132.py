3eccb34b1762be0b9add22d1bb3d7be5531b01ef
luisitcho <luishenrisc1@gmail.com>
2024-11-17 21:45:31 -0300

    curso python - aula 132

--- aula132.py
+++ aula132.py
@@ -1,6 +1,6 @@
 # Métodos em instâncias de classes Python
 # Hard coded - É algo que foi escrito diretamente no código
-class Carro():
+class Carro:
     def __init__(self, nome):
         self.nome = nome
 
