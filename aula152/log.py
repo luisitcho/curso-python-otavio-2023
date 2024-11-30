266d0afe76735310c196a4d5dd9ceaea68e2dfb7
luisitcho <luishenrisc1@gmail.com>
2024-11-30 18:12:19 -0300

    curso python - aula 152

--- aula152/log.py
+++ aula152/log.py
@@ -1,1 +1,39 @@
 # Abstração
+# Herança - é um
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o método log')
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        formatted_message = f'{msg} ({self.__class__.__name__})'
+        with open(LOG_FILE, 'a') as file:
+            file.write(formatted_message + '\n')
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+if __name__ == '__main__':
+    lp = LogPrintMixin()
+    lp.log_error('Qualquer coisa')
+    lp.log_success('Top')
+
+    lf = LogFileMixin()
+    lf.log_error('Qualquer coisa')
+    lf.log_success('Top')
+    # print(LOG_FILE)
