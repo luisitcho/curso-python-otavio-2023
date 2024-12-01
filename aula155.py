358ba15ac1af8abf29be3754a7351782ca7f63be
luisitcho <luishenrisc1@gmail.com>
2024-12-01 15:30:34 -0300

    curso python - aula 155

--- aula155.py
+++ aula155.py
@@ -11,3 +11,42 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload)  🐍 = ❌
+# Sobreposição de métodos (override) 🐍 = ✅
+from abc import ABC, abstractmethod
+
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('E-mail: enviando -', self.mensagem)
+        return True  # Simula envio com sucesso
+
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: enviando -', self.mensagem)
+        return False  # Simula envio com sucesso
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada com sucesso')
+    else:
+        print('Falha ao enviar notificação')
+
+
+notificacao_email = NotificacaoEmail('Testando mensagem de e-mail')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('Testando mensagem de SMS')
+notificar(notificacao_sms)
