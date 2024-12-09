7651bd2beae18285ee1520973b2f102df4493bc1
luisitcho <luishenrisc1@gmail.com>
2024-12-09 01:10:47 -0300

    curso python - aula 171

--- aula171.py
+++ aula171.py
@@ -5,7 +5,7 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
+from dataclasses import asdict, astuple, dataclass
 
 
 # @dataclass
@@ -16,6 +16,13 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
-    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
-    print(ordenadas)
+    p1 = Pessoa('Luis', 'Henrique')
+    # lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
+    # ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
+    # print(ordenadas)
+
+    print(p1)
+    print(asdict(p1).keys())
+    print(asdict(p1).values())
+    print(asdict(p1).items())
+    print(astuple(p1)[0])
