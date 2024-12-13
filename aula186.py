f613e12b0098b21620d5855eb183dde8ac7fd368
luisitcho <luishenrisc1@gmail.com>
2024-12-12 21:15:31 -0300

    curso python - aula 186

---
+++ aula186.py
@@ -0,0 +1,34 @@
+# os + shutil - Copiando arquivos com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+import os
+import shutil
+
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Área de Trabalho')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'Exemplos')
+NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+
+# print(HOME)
+# print(DESKTOP)
+# print(PASTA_ORIGINAL)
+# print(NOVA_PASTA)
+
+os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+
+    for dir_ in dirs:
+        caminho_novo_diretorio = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        print(caminho_novo_diretorio)
+        os.makedirs(caminho_novo_diretorio, exist_ok=True)
+
+    for file in files:
+        caminho_arquivo = os.path.join(root, file)
+        caminho_novo_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file)
+        shutil.copy(caminho_arquivo, caminho_novo_arquivo)
+        # print(caminho_arquivo)
+        # print(caminho_novo_arquivo)
