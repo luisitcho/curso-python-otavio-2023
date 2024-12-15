51a1b7f82a6432cca720f91a02d2fcba6b1c14dd
root <root@luisitcho.>
2024-12-14 22:19:35 -0300

    curso python - aula 188

--- aula188.py
+++ aula188.py
@@ -22,4 +22,39 @@
 # True          true
 # False         false
 # None          null
- 
+
+import json
+# from pprint import pprint
+from typing import TypedDict
+
+
+class Movie(TypedDict):
+    title: str
+    original_title: str
+    is_movie: bool
+    imdb_rating: float
+    year: int
+    characters: list[str]
+    budget: None | float
+
+
+string_json = '''
+    {
+        "title": "O Senhor dos Anéis: A Sociedade do Anel",
+        "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+        "is_movie": true,
+        "imdb_rating": 8.8,
+        "year": 2001,
+        "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+        "budget": null
+    } 
+'''
+
+filme: Movie = json.loads(string_json)
+# pprint(filme, indent=4)
+# print(filme['title'])
+# print(filme['characters'][0])
+# print(filme['year'] + 10)
+
+json_string = json.dumps(filme, indent=4, ensure_ascii=False)
+print(json_string)
