8118e6841f0e68e2c54d6b988a18066eb1238dd7
luisitcho <luishenrisc1@gmail.com>
2025-03-12 07:44:27 -0300

    curso python - aula 214 (Calculadora: adicionando ações nas teclas C, D e sinal de igual)

--- aula214_calculadora/display.py
+++ aula214_calculadora/display.py
@@ -26,21 +26,26 @@ class Display(QLineEdit):
         key = event.key()
         KEYS = Qt.Key
 
-        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
-        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
-        isEsc = key in [KEYS.Key_Escape]
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
 
         if isEnter:
+            print(f'EQ {text} pressionado, sinal emitido', type(self).__name__)
             self.eqPressed.emit()
             return event.ignore()
         # return super().keyPressEvent(event)
 
         if isDelete:
+            print('isDelete pressionado, sinal emitido', type(self).__name__)
+            print(f'isDelete  {text}  pressionado, sinal emitido',
+                  type(self).__name__)
             self.delPressed.emit()
             return event.ignore()
         # return super().keyPressEvent(event)
 
         if isEsc:
+            print('isEsc pressionado, sinal emitido', type(self).__name__)
             self.clearPressed.emit()
             return event.ignore()
         # return super().keyPressEvent(event)
