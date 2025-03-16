e852b4e346ece1fd0ad2f5ac630ce6c3714c965a
luisitcho <luishenrisc1@gmail.com>
2025-03-16 13:18:31 -0300

    curso python - criando o arquivo do SQLite (db.sqlte3)

---
+++ aula215/main.py
@@ -0,0 +1,12 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_PATH = ROOT_DIR / DB_NAME
+
+connection = sqlite3.connect(DB_PATH)
+cursor = connection.cursor()
+
+cursor.close()
+connection.close()
