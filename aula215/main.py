e9c8d9dcc30ed30c5948bb52b708510c7598257b
luisitcho <luishenrisc1@gmail.com>
2025-06-23 23:41:48 -0300

    cursor python - Criando minha primeira tabela no SQLite3

--- aula215/main.py
+++ aula215/main.py
@@ -5,9 +5,20 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_PATH = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_PATH)
 cursor = connection.cursor()
+
+cursor.execute(
+    f'''CREATE TABLE IF NOT EXISTS {TABLE_NAME}(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        weigth TEXT
+    )'''
+)
+
+connection.commit()
 
 cursor.close()
 connection.close()
